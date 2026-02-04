a8b5d9e0ee6a85a8658e60330cd034d48637135b
xirecentxz <129609019+xirecentxz@users.noreply.github.com>
2026-02-04 13:48:02 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  * v3.7.5 - Full Manual Selection Mode
  * Deskripsi: Awal upload tidak tercentang untuk mencegah file bengkak.
  */
-const APP_VERSION = "v3.7.6-ManualMode";
+const APP_VERSION = "v3.7.7-ManualMode";
 let excelData = [];
 
 // Inisialisasi Versi di Layar
