f2dcb9b59b4f944e1ec0550034957bdcc59c781c
xirecentxz <129609019+xirecentxz@users.noreply.github.com>
2026-02-04 13:44:00 +0700

    Update cleaner-blast.js

--- js/cleaner-blast.js
+++ js/cleaner-blast.js
@@ -1,6 +1,8 @@
 const blastCleaner = {
     format: function(phone) {
+        // Hapus semua selain angka
         let clean = String(phone).replace(/[^\d]/g, '');
+        
         // Normalisasi 08 atau 8 menjadi 628
         if (clean.startsWith('08')) clean = '62' + clean.slice(1);
         else if (clean.startsWith('8')) clean = '62' + clean;
